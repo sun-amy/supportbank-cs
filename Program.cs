a796d479bb25ee1234f536a0ea6fa4461c60c478
Amy <amysun.uk@gmail.com>
2024-08-07 13:44:33 +0100

    shows who owes how much and is owed how much.

--- Program.cs
+++ Program.cs
@@ -1,37 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-string transactionsStr;
-string filePath = @"Transactions2014.csv";
-
-using (StreamReader reader = new StreamReader(filePath)) 
+﻿namespace SupportBank
 {
-    transactionsStr = reader.ReadToEnd();
+    class Program
+    {
+        static IList<Transactions> transactionsClassArray = new List<Transactions>{};
+
+        static IList<Person> personClassArray = new List<Person>{};
+        static void Main(string[] args)
+        {
+            string transactionsStr;
+            string filePath = @"Transactions2014.csv";
+
+            using (StreamReader reader = new StreamReader(filePath)) 
+            {
+                transactionsStr = reader.ReadToEnd();
+            }
+
+
+            string[] transactionsArray = transactionsStr.Split("\n");
+            List<string> transactionsList = new List<string>(transactionsArray);
+            transactionsList.RemoveAt(0);
+
+            string[][] payments = [];
+
+            foreach(string payment in transactionsList)
+            {
+                payments = [.. payments, payment.Split(",")];
+            }
+
+            for (var i = 0; i < payments.Length; i++)
+            {
+                var payment = new Transactions {Date = payments[i][0], From = payments[i][1], To = payments[i][2], Narrative = payments[i][3], Amount = payments[i][4]};
+                transactionsClassArray = [.. transactionsClassArray, payment];
+            }
+
+            for (var i = 0; i < payments.Length; i++)
+            {
+                var payment = new Transactions {Date = payments[i][0], From = payments[i][1], To = payments[i][2], Narrative = payments[i][3], Amount = payments[i][4]};
+                transactionsClassArray = [.. transactionsClassArray, payment];
+            }
+
+            string[] peopleName = [];
+
+            foreach(string[] payment in payments) {
+                if (!peopleName.Contains(payment[1])) {
+                    peopleName = [.. peopleName, payment[1]];
+                }
+                if (!peopleName.Contains(payment[2])) {
+                    peopleName = [.. peopleName, payment[2]];
+                }
+            }
+            
+            for (var i = 0; i < peopleName.Length; i++)
+            {
+                var person = new Person {Name = peopleName[i]};
+                personClassArray = [.. personClassArray, person];
+            }
+
+            foreach (Person person in personClassArray) {
+                Console.WriteLine(person.Name + " owes £" + person.Owes() + " and is owed £" + person.Owed() + ".");
+            }
+        
+        }                 
+        public class Transactions {
+                public string? Date { get; set; }
+                public string? From { get; set; }
+                public string? To { get; set; }
+                public string? Narrative { get; set; }
+                public string? Amount { get; set; }
+            }
+
+        public class Person {
+            public string? Name { get; set; }
+
+            public string Owes() {
+                float owesTotal = 0;
+                foreach (Transactions transaction in transactionsClassArray)
+                {
+                    if (transaction.To == Name) {
+                        owesTotal += float.Parse(transaction.Amount);
+                    }
+                }
+                return Math.Round(owesTotal,2).ToString();
+            }
+
+            public string Owed() {
+                float owedTotal = 0;
+                foreach (Transactions transaction in transactionsClassArray)
+                {
+                    if (transaction.From == Name) {
+                        owedTotal += float.Parse(transaction.Amount);
+                    }
+                }
+                return Math.Round(owedTotal, 2).ToString();
+            }
+        }   
+        
+    }
 }
-
-string[] transactionsArray = transactionsStr.Split("\n");
-//Console.WriteLine(transactionsArray);
-
-string[] headers = transactionsArray[0].Split(",");
-Console.WriteLine(headers[0]);
-
-class Transactions {
-    public string date {get;}
-    public string from {get;}
-    public string to {get;}
-    public string narative {get;}
-    public string amount {get;}
-} 
-
-string[] transactionsClass =[];
-
-for (int i = 1; i < transactionsArray.Length; i++)
-{
-    var transaction = new Transactions() {transactionsArray[i][0], transactionsArray[i][1], transactionsArray[i][2], transactionsArray[i][3],transactionsArray[i][4]}
-    transactionsClass = [.. transactionsClass, transaction];
-}
-
-Console.WriteLine(transactionsClass[0].date);
-// foreach (string element in transactionsArray){
-//     Console.Write(element);
-// }
